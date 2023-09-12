bb997efa4044f837b65728e7c857e559b50a88e2
kerolosragaie <kerolosragaie@gmail.com>
2023-09-12 14:50:22 +0300

    setting up API services

--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -1,25 +1,84 @@
 import 'package:dio/dio.dart';
+import 'package:orange_bay_new/core/services/api_errors.dart';
 import 'package:provider/provider.dart';
 
-class ApiService{
-  ApiService({baseOptions, queryParams}) {
-    _queryParams = queryParams;
-    _baseOptions = baseOptions;
+class ApiService {
+  static final BaseOptions _baseOptions = BaseOptions(
+    responseType: ResponseType.json,
+    connectTimeout: const Duration(seconds: 10),
+    receiveTimeout: const Duration(seconds: 5),
+  );
+  final Dio _dio = Dio(_baseOptions);
+
+  ApiService([String? baseUrl]) {
+    if (baseUrl != null) {
+      _dio.options.baseUrl = baseUrl;
+    }
   }
 
-  static final Map<String, dynamic> headers = {
-    'Accept': 'application/json',
-  };
-  static Map<String, dynamic> _queryParams = {};
-  static BaseOptions _baseOptions = BaseOptions(
-      baseUrl: "",
-      headers: headers,
-      responseType: ResponseType.json,
-      contentType: 'application/json',
-      connectTimeout: const Duration(seconds: 5),
-      receiveTimeout: const Duration(seconds: 5),
-      queryParameters: _queryParams);
-  final Dio dio = Dio(_baseOptions);
+  Future<dynamic> get({
+    required String endPoint,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? query,
+  }) async {
+    try {
+      Response response = await _dio.get(endPoint);
+      return response.data;
+    } on DioException catch (error) {
+      throw ServerError.handleNetworkErrorType(error);
+    }
+  }
+
+  Future<dynamic> post({
+    required String endPoint,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? query,
+  }) async {
+    try {
+      Response response = await _dio.post(
+        endPoint,
+        data: body,
+        queryParameters: query,
+      );
+      return response.data;
+    } on DioException catch (error) {
+      throw ServerError.handleNetworkErrorType(error);
+    }
+  }
+
+  Future<dynamic> put({
+    required String endPoint,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? query,
+  }) async {
+    try {
+      Response response = await _dio.put(
+        endPoint,
+        data: body,
+        queryParameters: query,
+      );
+      return response.data;
+    } on DioException catch (error) {
+      throw ServerError.handleNetworkErrorType(error);
+    }
+  }
+
+  Future<dynamic> delete({
+    required String endPoint,
+    Map<String, dynamic>? body,
+    Map<String, dynamic>? query,
+  }) async {
+    try {
+      Response response = await _dio.delete(
+        endPoint,
+        data: body,
+        queryParameters: query,
+      );
+      return response.data;
+    } on DioException catch (error) {
+      throw ServerError.handleNetworkErrorType(error);
+    }
+  }
 }
 
 ApiService getApiServices(context) =>
