c2d01d6ed7fee5a5e69cdf41eb54e90508d8dffd
kerolosragaie <kerolosragaie@gmail.com>
2023-09-12 14:52:21 +0300

    providing api service to widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:orange_bay_new/core/services/api_service.dart';
+import 'package:orange_bay_new/core/services/api/api_service.dart';
 import 'package:provider/provider.dart';
 import 'features/splash/presentation/views/splash_view.dart';
 
 void main() {
+  final apiService = ApiService();
   runApp(
-    MultiProvider(providers: [
-      Provider<ApiService>(create: (context)=>ApiService()),
-    ], child: const MyApp()),
+    MultiProvider(
+      providers: [
+        Provider<ApiService>(create: (context) => apiService),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
