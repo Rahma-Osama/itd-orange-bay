131532240dbd64d0483fb6e0bb0fbeeff661c8e8
kerolosragaie <kerolosragaie@gmail.com>
2023-09-12 16:27:16 +0300

    making app stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -15,14 +15,9 @@ void main() {
   );
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
