3f6f713340adb5070aefbfeb4c66f7c0ea7cb09c
Rahma <rahmaosama2804@gmail.com>
2023-09-13 11:20:45 +0300

    add locale provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:orange_bay_new/core/localization/l10n.dart';
+import 'package:orange_bay_new/core/localization/provider/locale_provider.dart';
 import 'package:orange_bay_new/core/services/api/api_service.dart';
 import 'package:provider/provider.dart';
 import 'features/splash/presentation/views/splash_view.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   final apiService = ApiService();
@@ -20,9 +24,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
-    );
+    // final localeProvider = Provider.of<LocaleProvider>(context);
+    return ChangeNotifierProvider<LocaleProvider>(
+        create: (context) => LocaleProvider(),
+        builder: (context, s) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            locale: Provider.of<LocaleProvider>(context).locale,
+            supportedLocales: L10n.supportedLocales,
+            localizationsDelegates: const [
+              AppLocalizations.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            home: const SplashScreen(),
+          );
+        });
   }
 }
