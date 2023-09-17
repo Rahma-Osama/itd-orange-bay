8e5675dd1a90293a58499328f9fb8c9eb39ea6cb
Rahma <rahmaosama2804@gmail.com>
2023-09-17 14:18:33 +0300

    fix restaurant ui

--- lib/features/services/presentation/views/widgets/restaurant_container.dart
+++ lib/features/services/presentation/views/widgets/restaurant_container.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:orange_bay_new/core/constants/assets_data.dart';
 import 'package:orange_bay_new/core/theme/app_colors.dart';
-
-import '../../../../../core/theme/text_styles.dart';
+import 'package:orange_bay_new/core/theme/text_styles.dart';
 
 class RestContainer extends StatelessWidget {
   final int index;
@@ -11,13 +10,13 @@ class RestContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Container(
         clipBehavior: Clip.antiAliasWithSaveLayer,
         decoration: BoxDecoration(
             color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        height: MediaQuery.of(context).size.height * .45,
         child: Column(
           children: [
             Image.asset(
@@ -69,7 +68,7 @@ class RestContainer extends StatelessWidget {
                       ),
                       const Text('12:00 PM'),
                       SizedBox(
-                        width: MediaQuery.of(context).size.width / 10,
+                        width: size.width / 10,
                       ),
                       Icon(
                         Icons.arrow_forward,
@@ -77,7 +76,7 @@ class RestContainer extends StatelessWidget {
                       ),
                       const Text('3:00 PM'),
                       SizedBox(
-                        width: MediaQuery.of(context).size.width / 10,
+                        width: size.width / 10,
                       ),
                       Icon(
                         Icons.restaurant,
@@ -95,21 +94,16 @@ class RestContainer extends StatelessWidget {
                           fontWeight: FontWeight.w400),
                     ),
                   ),
-                  Row(
+                  const Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Text(
+                      Text(
                         "Start for 150.66 EGP / Person",
                         style: TextStyle(
                             color: Color(0xFF333333),
                             fontWeight: FontWeight.w500,
                             fontSize: 14),
                       ),
-                      Text("[Map]",
-                          style: TextStyles.textStyle14.copyWith(
-                            color: const Color(0XFF007489),
-                            fontWeight: FontWeight.w400,
-                          ))
                     ],
                   )
                 ],
