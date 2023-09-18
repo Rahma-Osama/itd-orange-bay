f9ff4a946b3c6b6b0dc69bff37846f2a79792f6d
Rahma <rahmaosama2804@gmail.com>
2023-09-19 01:16:11 +0300

    add gallery tabBar

--- lib/features/services/presentation/views/widgets/gallery_body.dart
+++ lib/features/services/presentation/views/widgets/gallery_body.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'package:orange_bay_new/data/constants.dart';
-import 'package:orange_bay_new/core/constants/assets_data.dart';
-import 'package:orange_bay_new/core/theme/app_colors.dart';
+import 'package:orange_bay_new/features/services/presentation/views/gallery_all_view.dart';
+import 'package:orange_bay_new/features/services/presentation/views/gallery_panoramic_view.dart';
+import 'package:orange_bay_new/features/services/presentation/views/gallery_relax_view.dart';
 
 class GalleryBody extends StatefulWidget {
   const GalleryBody({Key? key}) : super(key: key);
@@ -14,61 +13,99 @@ class GalleryBody extends StatefulWidget {
 class _GalleryBodyState extends State<GalleryBody> {
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Container(
-            height: 60,
-            padding: const EdgeInsets.all(8),
-            child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: 3,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          photoTypeIsselected = [false, false, false];
-                          photoTypeIsselected[index] = true;
-                        });
-                      },
-                      child: Container(
-                          decoration: BoxDecoration(
-                            borderRadius: photoTypeIsselected[index]
-                                ? BorderRadius.circular(10)
-                                : BorderRadius.circular(0),
-                            color: photoTypeIsselected[index]
-                                ? AppColors.deepOrange
-                                : Colors.white,
-                          ),
-                          alignment: Alignment.center,
-                          width: size.width * .3,
-                          child: Text(
-                            photoType[index],
-                            style: TextStyle(
-                                color: photoTypeIsselected[index]
-                                    ? Colors.white
-                                    : Colors.black),
-                          )),
-                    )),
-          ),
-          Expanded(
-            child: GridView.builder(
-                itemCount: photoTypeIsselected[0]?AssetsData.gallery.length : 5,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  mainAxisSpacing: size.height / 40,
-                  crossAxisSpacing: size.height / 40,
-                  childAspectRatio: .9,
-                  crossAxisCount: 2,
+    return const DefaultTabController(
+      length: 3,
+      child: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 45,
+                child: TabBar(
+                  tabs: [
+                    Tab(
+                      text: "All",
+                    ),
+                    Tab(
+                      child: Text(
+                        "Panoramic view",
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Tab(
+                      text: "Relax",
+                    ),
+                  ],
                 ),
-                itemBuilder: (context, index) => Image.asset(
-                      AssetsData.gallery[photoTypeIsselected[0]?index :photoTypeIsselected[1]?index+4 : index],
-                      fit: BoxFit.cover,
-                    )),
-          )
-        ],
-      ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: TabBarView(
+                    children: [
+                      GalleryAllView(),
+                      GalleryPanoramicView(),
+                      GalleryRelaxView(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 }
+
+// Column(
+// children: [
+// Container(
+// height: 60,
+// padding: const EdgeInsets.all(8),
+// child: ListView.builder(
+// physics: const BouncingScrollPhysics(),
+// itemCount: 3,
+// scrollDirection: Axis.horizontal,
+// itemBuilder: (context, index) => GestureDetector(
+// onTap: () {
+// setState(() {
+// photoTypeIsselected = [false, false, false];
+// photoTypeIsselected[index] = true;
+// });
+// },
+// child: Container(
+// decoration: BoxDecoration(
+// borderRadius: photoTypeIsselected[index]
+// ? BorderRadius.circular(10)
+// : BorderRadius.circular(0),
+// color: photoTypeIsselected[index]
+// ? AppColors.deepOrange
+//     : Colors.white,
+// ),
+// alignment: Alignment.center,
+// width: size.width * .3,
+// child: Text(
+// photoType[index],
+// style: TextStyle(
+// color: photoTypeIsselected[index]
+// ? Colors.white
+//     : Colors.black),
+// )),
+// )),
+// ),
+// Expanded(
+// child: GridView.builder(
+// itemCount: photoTypeIsselected[0]?AssetsData.gallery.length : 5,
+// gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+// mainAxisSpacing: size.height / 40,
+// crossAxisSpacing: size.height / 40,
+// childAspectRatio: .9,
+// crossAxisCount: 2,
+// ),
+// itemBuilder: (context, index) => Image.asset(
+// AssetsData.gallery[photoTypeIsselected[0]?index :photoTypeIsselected[1]?index+4 : index],
+// fit: BoxFit.cover,
+// )),
+// )
+// ],
+// ),
