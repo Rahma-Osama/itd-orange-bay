ce189d0b2d1221291c132efc12a884910bdf613c
Rahma <rahmaosama2804@gmail.com>
2023-09-20 14:22:45 +0300

    services localization

--- lib/features/services/presentation/views/widgets/services_container.dart
+++ lib/features/services/presentation/views/widgets/services_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:orange_bay_new/core/constants/assets_data.dart';
+import 'package:orange_bay_new/core/localization/l10n.dart';
 import 'package:orange_bay_new/core/theme/app_colors.dart';
 import 'package:orange_bay_new/core/theme/text_styles.dart';
 import 'package:orange_bay_new/features/services/presentation/views/activity_view.dart';
@@ -8,11 +9,29 @@ import 'package:orange_bay_new/features/services/presentation/views/videos_view.
 import 'package:orange_bay_new/features/services/presentation/views/gallery_view.dart';
 
 class ServicesContainer extends StatelessWidget {
+
   ServicesContainer({Key? key, required this.index}) : super(key: key);
   final int index;
 
   @override
   Widget build(BuildContext context) {
+    final locale = getL10n(context);
+    final List servicesHeaders = [
+      locale.restaurants,
+      locale.activities,
+      locale.videos,
+      locale.photos
+    ];
+    final List servicesData = [
+      '12 ${locale.restaurants}',
+      '8 ${locale.activities}',
+      '10 ${locale.videos}',
+      '40 ${locale.photos}'
+    ];
+
+
+    final size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () => Navigator.push(
           context, MaterialPageRoute(builder: (context) => screens[index])),
@@ -25,7 +44,7 @@ class ServicesContainer extends StatelessWidget {
           children: [
             Expanded(child: Image.asset(AssetsData.services[index])),
             Padding(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.height / 160),
+              padding: EdgeInsets.all(size.height / 160),
               child: Text(
                 servicesHeaders[index],
                 style: TextStyles.textStyle14
@@ -43,18 +62,6 @@ class ServicesContainer extends StatelessWidget {
     );
   }
 
-  final List servicesHeaders = [
-    'Restaurants',
-    'Activities',
-    'Videos',
-    'Photos'
-  ];
-  final List servicesData = [
-    '12 Restaurants',
-    '8 Activities',
-    '10 Videos',
-    '40 Photos'
-  ];
   final List screens = [
     const RestView(),
     const ActivityView(),
