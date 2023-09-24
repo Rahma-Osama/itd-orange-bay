9cb2c1bdd19cceb9bb99b41564764cc7b4564050
kerolosragaie <kerolosragaie@gmail.com>
2023-09-24 13:51:04 +0300

    fixing project files

--- lib/features/services/presentation/views/widgets/services_container.dart
+++ lib/features/services/presentation/views/widgets/services_container.dart
@@ -1,35 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:orange_bay_new/core/constants/assets_data.dart';
+import 'package:orange_bay_new/core/constants/features/services/constants.dart';
 import 'package:orange_bay_new/core/localization/l10n.dart';
 import 'package:orange_bay_new/core/theme/app_colors.dart';
 import 'package:orange_bay_new/core/theme/text_styles.dart';
+import 'package:orange_bay_new/data/constants.dart';
 import 'package:orange_bay_new/features/services/presentation/views/activity_view.dart';
 import 'package:orange_bay_new/features/services/presentation/views/restaurant_view.dart';
 import 'package:orange_bay_new/features/services/presentation/views/videos_view.dart';
 import 'package:orange_bay_new/features/services/presentation/views/gallery_view.dart';
 
 class ServicesContainer extends StatelessWidget {
-
   ServicesContainer({Key? key, required this.index}) : super(key: key);
   final int index;
 
   @override
   Widget build(BuildContext context) {
     final locale = getL10n(context);
-    final List servicesHeaders = [
-      locale.restaurants,
-      locale.activities,
-      locale.videos,
-      locale.photos
-    ];
-    final List servicesData = [
-      '12 ${locale.restaurants}',
-      '8 ${locale.activities}',
-      '10 ${locale.videos}',
-      '40 ${locale.photos}'
-    ];
-
-
+    final servicesData = getServicesData(locale);
+    final servicesHeaders = getServicesHeaders(locale);
     final size = MediaQuery.of(context).size;
 
     return GestureDetector(
@@ -38,8 +27,9 @@ class ServicesContainer extends StatelessWidget {
       child: Container(
         padding: const EdgeInsets.all(20),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10), color: Colors.white),
-        // height: MediaQuery.of(context).size.height/3,
+          borderRadius: BorderRadius.circular(10),
+          color: Colors.white,
+        ),
         child: Column(
           children: [
             Expanded(child: Image.asset(AssetsData.services[index])),
@@ -67,7 +57,5 @@ class ServicesContainer extends StatelessWidget {
     const ActivityView(),
     const VideosView(),
     const GalleryView(),
-    // const MemberShipView(),
-    // const shopView()
   ];
 }
